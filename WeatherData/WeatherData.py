10f74da891c643bee6ec3a66dc7eb86f05b1101a
Michael Usachenko <senseikawasaki@gmail.com>
2017-11-09 21:21:27 -0600

    massive overhaul of WeatherData.py

--- WeatherData/WeatherData.py
+++ WeatherData/WeatherData.py
@@ -5,61 +5,40 @@ import sys
 class WeatherData(object):
 
 	def __init__(self, cleanedName, mainDataTypes=[]):
-		self.cleanedName = cleanedName.split('_')[1]
-		self.cleanedDF = pd.read_csv((cleanedName +'.csv'))
-		self.baseColumns = ['STATION_CODE','YEAR']
-		self.mainDataTypes = list(set(['PRCP','SNOW','SNWD','TMAX','TMIN']+mainDataTypes))
+		self.cleanedName = (cleanedName.split('_')[1]).split('.')[0]
+		self.cleanedDF = pd.read_csv((cleanedName))
+		self.baseColumns = ['STATION_CODE','YEAR', "MONTH", "DAY"]
+		self.mainDataTypes = ['PRCP','SNOW','SNWD','TMAX','TMIN','TAVG']+mainDataTypes
 		self.stationStatHeaders = ['LATITUDE','LONGITUDE','ELEVATION','STATION_NAME']
 		self.stationStats = None
 
 		self.code = self.cleanedDF.iloc[0]['V1']
-		self.years = ((self.cleanedDF.iloc[1]).tolist())[1:]
+		self.years = sorted(((self.cleanedDF.iloc[1]).tolist())[1:])
 		self.months = ((self.cleanedDF.iloc[2]).tolist())[1:]
+		self.months = [m.strip() for m in self.months]
 		self.sensors = ((self.cleanedDF.iloc[3]).tolist())[1:]
-		self.ymsTuples = [(y,m,s) for y,m,s in zip(self.years, self.months, self.sensors)]	
-		self.firstUsage = True
+		self.ymsTuples = [(y,m,s) for y,m,s in zip(self.years, self.months, self.sensors)]
+		self.daysList = [day+1 for day in range(31)]
 
 	# ---------- USER LEVEL FUNCTIONS -----------------
+
 	def getFullCSV(self): 
-		self.firstTimeUseCheck()
-		fullDF, firstLoop = None, True
-
-		for year in set(self.years):
-			for month in sorted(set(self.months)):
-				if(firstLoop):
-					firstLoop = False
-					fullDF = self.createFormattedCSV(year, month, True)
-				else:
-					monthDF = self.createFormattedCSV(year, month, True)
-					fullDF = fullDF.append(monthDF, ignore_index=True)
-
-		csvName = self.code + '_' + str(year) + '.csv'
-		fullDF.to_csv(csvName, encoding='utf-8', index=False)
+		self.formatCleanedDF()
+		self.getStationData()
+		rowData = self.retrieveRowData()
+		df = self.generateFullDataframe(rowData)
+		df.to_csv(str(self.code)+".csv", encoding='utf-8', index=False)
 
 	# ---------- FORMATTING FUNCTIONS -----------------
-	def firstTimeUseCheck(self):
-		if (self.firstUsage):
-			self.firstUsage = False
-			self.formatCleanedDF()
-			self.getStationData()
 
 	def getStationData(self):
 		import csv
-		with open('stations.csv') as stations:
+		with open("stations.csv") as stations:
 		    csvReader = csv.reader(stations)
 		    for row in csvReader:
 		        if (row[0] == self.cleanedName):
 		        	self.stationStats = tuple(row)[1:]
 		        	break
-
-	def createFormattedCSV(self, year, month, appendMode=False):
-		emptyDF = self.createEmptyDataframe()
-		populatedDF = self.populateDataframeSensors(emptyDF, year, month)
-		if (appendMode == False):
-			csvName = self.code + '_' + str(year) + '_M' + str(month)+'.csv'
-			populatedDF.to_csv(csvName, encoding='utf-8', index=False)
-		else:
-			return populatedDF
 
 	def createColumnHeaders(self, tuplesList):
 		return [str(yms[0]+"_"+yms[1]+"_"+yms[2]) for yms in tuplesList]
@@ -70,31 +49,49 @@ class WeatherData(object):
 		self.cleanedDF.columns = self.createColumnHeaders(self.ymsTuples)
 
 	# ---------- DATAFRAME GENERATION FUNCTIONS -----------------
-	def createEmptyDataframe(self):
-		
-		days, uniqueYears = 31, set(self.years)
-		dfColumns = self.baseColumns + self.stationStatHeaders + self.mainDataTypes
-		allYearsInDF = [[year]*days for year in uniqueYears]
-		allYearsInDF = list(itertools.chain.from_iterable(allYearsInDF))
-		baseTuples = [(self.code, year) for year in allYearsInDF]
 
-		naTuples = [('NA',)*(len(self.mainDataTypes)) for i in range(days*len(uniqueYears))]
-		stationTuples = [self.stationStats for i in range(days*len(uniqueYears))]
-		dfTuples = [tuple(i+j+k) for i,j,k in zip(baseTuples, stationTuples, naTuples)]
+	def generateFullDataframe(self, rowData):
+		dataframe = pd.DataFrame(rowData, columns=(self.baseColumns+self.stationStatHeaders+self.mainDataTypes))
+		dataframe.fillna("NA", inplace=True)
+		return dataframe
 
-		return pd.DataFrame(dfTuples, columns=dfColumns)
+	def orderByDataType(self, order, rawTuple):
+		correctOrder = [self.mainDataTypes.index(sensor) for sensor in order]
+		finalTuple = ['NA',]*len(self.mainDataTypes)
+		for idx, val in zip(correctOrder, list(rawTuple)):
+			try:
+				finalTuple[idx] = str(int(val))
+			except ValueError:
+				continue
 
-	def populateDataframeSensors(self, emptyDF, year, month): 
-		
-		inputMonthSensors = list(filter(lambda x: x[1] == str(month), self.ymsTuples))
-		validSensors = list(filter(lambda x: x[2] in self.mainDataTypes, inputMonthSensors))
+		return tuple(finalTuple)
+
+	def retrieveRowData(self): 
+
+		validSensors = list(filter(lambda x: x[2] in self.mainDataTypes, self.ymsTuples))
 		cleanedTargetColumns = self.createColumnHeaders(validSensors)
-		populatedColumns = [self.cleanedDF[target].tolist() for target in cleanedTargetColumns]
+		
+		columnsAndYears = [(column, year) for column, year in zip(cleanedTargetColumns, validSensors)]
+		formattedYears = [list(filter(lambda x: x[1][0] == year, columnsAndYears)) for year in sorted(set(self.years))]
+		monthsAsInt = sorted([int(m) for m in set(self.months)])
 
-		for column, sensor in zip(populatedColumns, validSensors):
-			emptyDF[sensor[2]] = column
+		orderedRowData = []
+		for year in formattedYears:
+			allMonths = [list(filter(lambda x: x[1][1] == str(month), year)) for month in monthsAsInt]
+			allMonths = [month for month in allMonths if month != []]
+			
+			for month in allMonths:
+				allSensorData = [self.cleanedDF[sensorType[0]].tolist() for sensorType in month]
+				allSensorData = list(zip(*allSensorData))
+				sensorOrder = [sensorType[1][2] for sensorType in month]
 
-		return emptyDF[emptyDF['YEAR'] == str(year)]
+				stationTimeTuple = (self.code, month[0][1][0], month[0][1][1])
+				stationData = [stationTimeTuple+(days,)+self.stationStats for days in self.daysList]
+				formattedRowData = [self.orderByDataType(sensorOrder, sensorTup) for sensorTup in allSensorData]
+				mergedData = [sData+rData for sData, rData in zip(stationData, formattedRowData)]
+				orderedRowData.append(mergedData)
+
+		return list(itertools.chain.from_iterable(orderedRowData))
 
 # -------- SCRIPT EXECUTION BELOW ------------------------
 
@@ -105,4 +102,3 @@ if __name__ == "__main__":
 	else:
 		data = WeatherData(cleanedName=str(sys.argv[1]))
 		data.getFullCSV()
-
